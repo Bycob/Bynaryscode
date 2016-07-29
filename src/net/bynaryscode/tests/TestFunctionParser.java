756444677e92122b57a5c7e97bb05dae6c2dfede
BynaryCobweb <lsj456@gmail.com>
2016-07-29 18:55:50 +0200

    Parser pour les fonctions polynomiales + tests unitaires

---
+++ src/net/bynaryscode/tests/TestFunctionParser.java
@@ -0,0 +1,51 @@
+/* <LICENSE>
+Copyright 2015-2016 Louis JEAN
+
+This file is part of BynarysCode.
+
+BynarysCode is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BynarysCode is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BynarysCode. If not, see <http://www.gnu.org/licenses/>.
+ </LICENSE> */
+
+package net.bynaryscode.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import net.bynaryscode.util.maths.analysis.FunctionParser;
+import net.bynaryscode.util.maths.analysis.PolynomialFunction;
+
+public class TestFunctionParser {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testParsePolynomialFunctionNotNull() {
+		assertNotNull(FunctionParser.parsePolynomial(""));
+	}
+	
+	@Test
+	public void testParsePolynomialFunction() {
+		PolynomialFunction function = new PolynomialFunction(3, 2, 5.55);
+		assertEquals(function.toString(), FunctionParser.parsePolynomial("").toString());
+	}
+}
--- src/net/bynaryscode/util/maths/analysis/FunctionParser.java
+++ src/net/bynaryscode/util/maths/analysis/FunctionParser.java
@@ -19,10 +19,41 @@ along with BynarysCode. If not, see <http://www.gnu.org/licenses/>.
 
 package net.bynaryscode.util.maths.analysis;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FunctionParser {
 	
-	public PolynomialFunction parsePolynomial(String function) {
-		String regex = "(^[\\+-]?|[\\+-])\\s*(\\d*\\.?\\d*)(\\s*x(\\^(\\d+))?)?";
-		return null;
+	public static PolynomialFunction parsePolynomial(String function) {
+		String regex = "(^[\\+-]?|[\\+-])\\s*(\\d*\\.?\\d*)\\s*(x(\\^(\\d+))?)?";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(function);
+		
+		PolynomialFunction result = new PolynomialFunction();
+		while (matcher.find()) {
+			if ("".equals(matcher.group())) {
+				continue;
+			}
+			
+			//Détermination de la puissance
+			String powerStr = matcher.group(5);
+			int power = 0;
+			if (!"".equals(powerStr) && powerStr != null) {
+				power = Integer.parseInt(powerStr);
+			}
+			else if ("x".equals(matcher.group(3))) {
+				power = 1;
+			}
+			//else power = 0
+			
+			//Coefficient
+			if (".".equals(matcher.group(2))) {
+				throw new IllegalArgumentException("La fonction passée en paramètres n'est pas valide");
+			}
+			String coefStr = matcher.group(1) + matcher.group(2);
+			
+			result.setCoefficient(power, result.getCoefficient(power) + Double.parseDouble(coefStr));
+		}
+		return result;
 	}
 }
